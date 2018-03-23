0fc56aa3e5c0a5480f73a76e91531be824ddd0b2
yannikschaelte <yannik.schaelte@gmail.com>
2018-03-23 17:13:13 +0100

    reshape and test fo ~isfinite

--- hieropt_c_normal.m
+++ hieropt_c_normal.m
@@ -28,15 +28,17 @@ end
 
 arr_y = reshape(arr_y,1,[]);
 arr_h = reshape(arr_h,1,[]);
+arr_b = reshape(arr_b,1,[]);
 arr_recnoise = reshape(1./arr_noise,1,[]); % reciprocal noise
 
 % unset values if not defined
-bad_indices = ~isfinite(arr_y) | ~isfinite(arr_h) | ~isfinite(arr_recnoise);
+bad_indices = ~isfinite(arr_y) | ~isfinite(arr_h) | ~isfinite(arr_recnoise) | ~isfinite(arr_b);
 arr_y(bad_indices) = 0;
 arr_h(bad_indices) = 0;
+arr_b(bad_indices) = 0;
 arr_recnoise(bad_indices) = 0; % here 1/inf = 0
 
-sum_yh = nansum((arr_y -arr_b) .* arr_h .* arr_recnoise);
+sum_yh = nansum((arr_y - arr_b) .* arr_h .* arr_recnoise);
 sum_h2 = nansum(arr_h.^2 .*arr_recnoise);
 
 c = sum_yh / sum_h2;
